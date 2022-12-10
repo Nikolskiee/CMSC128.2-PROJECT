ac96d8f9679881b6bc70283c2b19e67adfb00c53
Romaine Dara Regala <rmregala@up.edu.ph>
2022-12-10 16:51:15 +0800

    Fiinished integrating html in django framework
    Finished integrating html and css files in  django framework.
    
    Working on the user account creation (login and signup).

--- seirvice/backend/urls.py
+++ seirvice/backend/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.home, name='home'),
+    path('history/', views.history, name='history'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- seirvice/backend/views.py
+++ seirvice/backend/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+#@login_required(login_url='/login')
+#i-uncomment niyo na lang yung login required kung bet niyo matest ibang html working pa sa user
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'simulation.html')
+
+def history(request):
+    return render(request, 'history.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
